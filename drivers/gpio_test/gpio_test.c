62f39555c55b14cf8ce61db32ad23cfc6c2e2c9b
Denny Yang <denny.ds.yang@gmail.com>
2011-03-16 22:45:10 -0700

    Add gpio test program

---
+++ drivers/gpio_test/gpio_test.c
@@ -0,0 +1,146 @@
+/*
+ * GPIO test program
+ *
+ * Denny (Dongsung) Yang
+ *
+ * Licensed under the GPL-2 or later
+ */
+ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#define LED1 132
+#define LED2 133
+#define LED3 134
+#define LED4 135
+ 
+#define GPIO_DIR_IN	0
+#define GPIO_DIR_OUT	1
+ 
+int gpio_export(unsigned gpio)
+{
+	int fd, len;
+	char buf[11];
+ 
+	fd = open("/sys/class/gpio/export", O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/export");
+		return fd;
+	}
+ 
+	len = snprintf(buf, sizeof(buf), "%d", gpio);
+	write(fd, buf, len);
+	close(fd);
+ 
+	return 0;
+}
+ 
+int gpio_unexport(unsigned gpio)
+{
+	int fd, len;
+	char buf[11];
+ 
+	fd = open("/sys/class/gpio/unexport", O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/export");
+		return fd;
+	}
+ 
+	len = snprintf(buf, sizeof(buf), "%d", gpio);
+	write(fd, buf, len);
+	close(fd);
+	return 0;
+}
+ 
+int gpio_dir(unsigned gpio, unsigned dir)
+{
+	int fd, len;
+	char buf[60];
+ 
+	len = snprintf(buf, sizeof(buf), "/sys/class/gpio/gpio%d/direction", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/direction");
+		return fd;
+	}
+ 
+	if (dir == GPIO_DIR_OUT)
+		write(fd, "out", 4);
+	else
+		write(fd, "in", 3);
+ 
+	close(fd);
+	return 0;
+}
+ 
+int gpio_dir_out(unsigned gpio)
+{
+	return gpio_dir(gpio, GPIO_DIR_OUT);
+}
+ 
+int gpio_dir_in(unsigned gpio)
+{
+	return gpio_dir(gpio, GPIO_DIR_IN);
+}
+ 
+int gpio_value(unsigned gpio, unsigned value)
+{
+	int fd, len;
+	char buf[60];
+ 
+	len = snprintf(buf, sizeof(buf), "/sys/class/gpio/gpio%d/value", gpio);
+ 
+	fd = open(buf, O_WRONLY);
+	if (fd < 0) {
+		perror("gpio/value");
+		return fd;
+	}
+ 
+	if (value)
+		write(fd, "1", 2);
+	else
+		write(fd, "0", 2);
+ 
+	close(fd);
+	return 0;
+}
+
+void led_control(unsigned value)
+{
+	if (value & 0x01)
+		gpio_value(LED1, 0);	
+	else
+		gpio_value(LED1, 1);
+
+	if (value & 0x02)
+		gpio_value(LED2, 0);	
+	else
+		gpio_value(LED2, 1);
+
+	if (value & 0x04)
+		gpio_value(LED3, 0);	
+	else
+		gpio_value(LED3, 1);
+
+	if (value & 0x08)
+		gpio_value(LED4, 0);	
+	else
+		gpio_value(LED4, 1);
+}
+ 
+int main(void)
+{
+	int i = 10000;
+
+
+	while(i--) {
+		led_control(i & 0x0F);
+		usleep(100000);
+	}
+	return 0;
+}
+
--- drivers/trainer_gpio.c
+++ drivers/trainer_gpio.c
@@ -18,12 +18,7 @@
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Denny Dongsung Yang");
 
-static short int myshort = 1;
-static int myint = 420;
-static long int mylong = 9999;
-static char *mystring = "blah";
-static int myintArray[2] = { -1, -1 };
-static int arr_argc = 0;
+static int debug = 0;
 
 /* 
  * module_param(foo, int, 0000)
@@ -33,25 +28,8 @@ static int arr_argc = 0;
  * for exposing parameters in sysfs (if non-zero) at a later stage.
  */
 
-module_param(myshort, short, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
-MODULE_PARM_DESC(myshort, "A short integer");
-module_param(myint, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-MODULE_PARM_DESC(myint, "An integer");
-module_param(mylong, long, S_IRUSR);
-MODULE_PARM_DESC(mylong, "A long integer");
-module_param(mystring, charp, 0000);
-MODULE_PARM_DESC(mystring, "A character string");
-
-/*
- * module_param_array(name, type, num, perm);
- * The first param is the parameter's (in this case the array's) name
- * The second param is the data type of the elements of the array
- * The third argument is a pointer to the variable that will store the number 
- * of elements of the array initialized by the user at module loading time
- * The fourth argument is the permission bits
- */
-module_param_array(myintArray, int, &arr_argc, 0000);
-MODULE_PARM_DESC(myintArray, "An array of integers");
+module_param(debug, int, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP);
+MODULE_PARM_DESC(debug, "debug");
 
 static int __init trainer_gpio_init(void)
 {
@@ -60,18 +38,6 @@ static int __init trainer_gpio_init(void)
         gpio_export(130, 1);
         gpio_request(131, "sysfs");
         gpio_export(131, 1);
-        gpio_request(132, "sysfs");
-        gpio_export(132, 1);
-        gpio_request(133, "sysfs");
-        gpio_export(133, 1);
-        gpio_request(134, "sysfs");
-        gpio_export(134, 1);
-        gpio_request(135, "sysfs");
-        gpio_export(135, 1);
-        gpio_request(136, "sysfs");
-        gpio_export(136, 1);
-        gpio_request(137, "sysfs");
-        gpio_export(137, 1);
         gpio_request(138, "sysfs");
         gpio_export(138, 1);
         gpio_request(139, "sysfs");
@@ -83,12 +49,68 @@ static int __init trainer_gpio_init(void)
         gpio_request(162, "sysfs");
         gpio_export(162, 1);
 
+	/* GPIO 132~135, 136, 137 */
+	if ((gpio_request(132, "GPIO132") == 0) &&
+	    (gpio_direction_output(132, 1) == 0)) {
+		gpio_export(132, 0);
+	} else {
+		printk(KERN_ERR "could not obtain gpio for GPIO132\n");
+	}
+
+	if ((gpio_request(133, "GPIO133") == 0) &&
+	    (gpio_direction_output(133, 1) == 0)) {
+		gpio_export(133, 0);
+	} else {
+		printk(KERN_ERR "could not obtain gpio for GPIO133\n");
+	}
+
+	if ((gpio_request(134, "GPIO134") == 0) &&
+	    (gpio_direction_output(134, 1) == 0)) {
+		gpio_export(134, 0);
+	} else {
+		printk(KERN_ERR "could not obtain gpio for GPIO134\n");
+	}
+
+	if ((gpio_request(135, "GPIO135") == 0) &&
+	    (gpio_direction_output(135, 1) == 0)) {
+		gpio_export(135, 0);
+	} else {
+		printk(KERN_ERR "could not obtain gpio for GPIO135\n");
+	}
+
+	if ((gpio_request(136, "GPIO136") == 0) &&
+	    (gpio_direction_output(136, 1) == 0)) {
+		gpio_export(136, 0);
+	} else {
+		printk(KERN_ERR "could not obtain gpio for GPIO136\n");
+	}
+
+	if ((gpio_request(137, "GPIO137") == 0) &&
+	    (gpio_direction_output(137, 1) == 0)) {
+		gpio_export(137, 0);
+	} else {
+		printk(KERN_ERR "could not obtain gpio for GPIO137\n");
+	}
+
 	return 0;
 }
 
 static void __exit trainer_gpio_exit(void)
 {
-	printk(KERN_INFO "Exit of trainer_gpio\n");
+        printk(KERN_INFO "Beagle expansionboard: Unexporting GPIOs 130-141,162 to userspace\n");
+        gpio_unexport(130);
+        gpio_unexport(131);
+        gpio_unexport(132);
+        gpio_unexport(133);
+        gpio_unexport(134);
+        gpio_unexport(135);
+        gpio_unexport(136);
+        gpio_unexport(137);
+        gpio_unexport(138);
+        gpio_unexport(139);
+        gpio_unexport(140);
+        gpio_unexport(141);
+        gpio_unexport(162);
 }
 
 module_init(trainer_gpio_init);
